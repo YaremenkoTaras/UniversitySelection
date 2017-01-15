feea94c882aed941b34deea9f5b4a00b01256914
Tapac <yaremenko.taras@gmail.com>
2017-01-15 19:59:46 +0200

    Command ShowFaculty updated

---
+++ src/main/java/com/epam/autum/selection/command/CommandAddApplication.java
@@ -0,0 +1,18 @@
+package com.epam.autum.selection.command;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Tapac on 15.01.2017.
+ */
+public class CommandAddApplication implements ICommand{
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse responce) throws ServletException, IOException {
+
+
+        return null;
+    }
+}
