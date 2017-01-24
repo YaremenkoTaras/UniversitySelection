cb0e8f369bee03bbcc3a27f67de49ab48e180050
Taras <yaremenko.taras@gmail.com>
2017-01-24 14:40:19 +0200

    Revised.

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -2,15 +2,15 @@ INSERT INTO user_role (role) VALUES ('ADMIN'),('USER'),('GUEST');
 
 INSERT INTO application_status (status) VALUES ('ACCEPT'),('PROCESS'),('DECLINE');
 
-INSERT INTO faculty (name, number_of_student, short_name) VALUES ('Aerospace system faculty',20,'ASSF');
-INSERT INTO faculty (name, number_of_student, short_name) VALUES ('Information technology Faculty',20,'ITF');
+INSERT INTO faculty (name, number_of_student, short_name) VALUES ('Aerospace System Faculty',20,'ASSF');
+INSERT INTO faculty (name, number_of_student, short_name) VALUES ('Information Technology Faculty',20,'ITF');
 INSERT INTO faculty (name, number_of_student, short_name) VALUES ('Applied System Analysis',20,'ASA');
 
-INSERT INTO user (name,sex,birth,phone,address,email,password,user_role_id) VALUES ('Yaremenko Taras','M','1992-12-12','0934326424','Kyiv','yaremenko.taras@gmail.com',1111,1);
 INSERT INTO user (name,sex,birth,phone,address,email,password,user_role_id) VALUES ('Yarema Bohdan','M','1992-12-12','0983332244','Kyiv','user1@email.com',1111,2);
 INSERT INTO user (name,sex,birth,phone,address,email,password,user_role_id) VALUES ('Zdolbunov Vitaliy','M','1997-12-12','0983332244','Odessa','user2@email.com',1111,2);
 INSERT INTO user (name,sex,birth,phone,address,email,password,user_role_id) VALUES ('Opanasenko Evgeniy','M','1996-4-4','0983332244','Kyiv','user3@email.com',1111,2);
 INSERT INTO user (name,sex,birth,phone,address,email,password,user_role_id) VALUES ('Teslya Yaroslav','M','1996-6-17','0983332244','Lviv','user4@email.com',1111,2);
+INSERT INTO user (name,sex,birth,phone,address,email,password,user_role_id) VALUES ('Yaremenko Taras','M','1992-12-12','0934326424','Kyiv','yaremenko.taras@gmail.com',1111,1);
 
 INSERT INTO subject (name) VALUES ('Certifycate'),('Mathematic'), ('Physics'), ('Language'), ('History');
 
@@ -60,8 +60,8 @@ INSERT INTO application (date, description, overall, faculty_id, user_id, applic
 
 INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',712,1,3,2);
 INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',712,2,3,2);
-INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',899,2,3,2);
+INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',899,3,3,2);
 
 INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',746,1,4,2);
 INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',746,2,4,2);
-INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',932,2,4,2);
+INSERT INTO application (date, description, overall, faculty_id, user_id, application_status_id) VALUES   ('2017-01-01','KPI',932,3,4,2);
