9e011fcb9affe5ef969b9db88eeb73e925ecce29
Taras <yaremenko.taras@gmail.com>
2017-01-24 12:38:02 +0200

    Revised.

--- src/main/java/com/epam/autum/selection/command/CommandChangeUserInfo.java
+++ src/main/java/com/epam/autum/selection/command/CommandChangeUserInfo.java
@@ -16,8 +16,8 @@ import java.io.IOException;
 /**
  * Created by Tapac on 18.01.2017.
  */
-public class CommandChangeUser implements ICommand {
-    private static Logger log = LogManager.getLogger(CommandChangeUser.class);
+public class CommandChangeUserInfo implements ICommand {
+    private static Logger log = LogManager.getLogger(CommandChangeUserInfo.class);
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -25,7 +25,6 @@ public class CommandChangeUser implements ICommand {
         String add = request.getParameter(ADDITIONAL);
         switch (add) {
             case "edit":
-                loadEditAttribute(request, response);
                 page = PageConfigurator.getConfigurator().getPage(PageConfigurator.CHANGE_USER_INFORMATION_PAGE);
                 break;
             case "submit":
@@ -38,16 +37,8 @@ public class CommandChangeUser implements ICommand {
         return page;
     }
 
-    private void loadEditAttribute(HttpServletRequest request, HttpServletResponse response) {
-        User user = (User)request.getSession().getAttribute(USER);
-        request.setAttribute(PARAMETER_ADDRESS, user.getAddress());
-        request.setAttribute(PARAMETER_EMAIL, user.getEmail());
-        request.setAttribute(PARAMETER_PHONE, user.getPhone());
-    }
-
     private String updateUser(HttpServletRequest request, HttpServletResponse response) {
         String page;
-        User user = (User) request.getSession().getAttribute(USER);
         ValidationResult result = ValidationResult.UNKNOWN_ERROR;
 
         String address = request.getParameter(PARAMETER_ADDRESS);
@@ -55,37 +46,38 @@ public class CommandChangeUser implements ICommand {
         String phone = request.getParameter(PARAMETER_PHONE);
         String pass = request.getParameter(PARAMETER_PASSWORD);
         String newPass = request.getParameter(PARAMETER_PASSWORD_TWO);
+
+        User user = (User) request.getSession().getAttribute(USER);
         User newUser = null;
-        try {
-            newUser = UserLogic.checkEmail(user.getEmail(), pass);
-            if (newUser != null) {
-                newUser.setAddress(address);
-                newUser.setPhone(phone);
-                newUser.setPassword(newPass);
-                if (newUser.getEmail().equals(email)) {
-                    result = UserLogic.editUser(newUser);
-                }else if(UserLogic.isEmailFree(email)){
-                    newUser.setEmail(email);
-                    result = UserLogic.editUser(newUser);
-                }else {
-                    result = ValidationResult.EMAIL_NOT_UNIQUE;
+        if (user != null)
+            try {
+                newUser = UserLogic.checkEmail(user.getEmail(), pass);
+                if (newUser != null) {
+                    newUser.setAddress(address);
+                    newUser.setPhone(phone);
+                    newUser.setPassword(newPass);
+                    if (newUser.getEmail().equals(email)) {
+                        result = UserLogic.editUser(newUser);
+                    } else if (UserLogic.isEmailFree(email)) {
+                        newUser.setEmail(email);
+                        result = UserLogic.editUser(newUser);
+                    } else {
+                        result = ValidationResult.EMAIL_NOT_UNIQUE;
+                    }
                 }
+            } catch (LogicException e) {
+                log.error(e);
             }
-        } catch (LogicException e) {
-            log.error(e);
-        }
         switch (result) {
             case ALL_RIGHT:
-                request.getSession().setAttribute(USER,newUser);
+                request.getSession().setAttribute(USER, newUser);
                 page = (new CommandShowApplicant()).execute(request, response);
                 break;
             case EMAIL_NOT_UNIQUE:
-                loadEditAttribute(request, response);
                 request.setAttribute(DESCRIPTION, ValidationResult.EMAIL_NOT_UNIQUE);
                 page = PageConfigurator.getConfigurator().getPage(PageConfigurator.CHANGE_USER_INFORMATION_PAGE);
                 break;
             default:
-                loadEditAttribute(request, response);
                 page = PageConfigurator.getConfigurator().getPage(PageConfigurator.CHANGE_USER_INFORMATION_PAGE);
                 break;
         }
